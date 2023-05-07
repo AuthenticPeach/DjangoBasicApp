3fb7785df23f45d5e33069f2713558cf15d5663f
AuthenticPeach <51739720+AuthenticPeach@users.noreply.github.com>
2023-05-07 13:21:15 -0700

    Simple Database - Python, Django
    Books database
    - also added notes to help ease back into Django

---
+++ books/reading/README Python-Django.py
@@ -0,0 +1,12 @@
+# README Python-Django 
+def multiline_example():
+# Hello Future-self, Welcome back to Django
+# If you are reading this, then you want to return to databases with Django.
+# 1. So Django works by importing its module into your project with pip "python -m pip install django", (after step 4)
+# 2. First you run the program with a 2nd powershell terminal.
+# 3. We are also using virtualenv to keep things seperate, pip install virtualenv | virtualenv env
+# 4. You have to activate your environment with ".\env\Scripts\activate" - Scripts is the example folder, "deactivate" ends int
+# 5. To start a project - "django-admin startproject books"
+# 6. python manage.py shell  | or replace "shell" with "runserver"
+# 7. And then to refresh your terminal (which is mandatory), you run "exit()"
+# 8. Then you retarget your project "from reading.models import Book"
